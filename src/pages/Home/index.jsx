d0d9f0a5d8259f1f923b821dd4b3e3787c3e9844
Yitbarek Birhanie <47268052+YitbarekB@users.noreply.github.com>
2022-04-19 14:39:32 +0300

    Add files via upload

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,1 +1,46 @@
+import './index.scss'
+import '../../components/AnimatedLetters/index.scss'
+import LogoTitle from '../../assets/images/eger_logo.svg'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../../components/AnimatedLetters'
+import Logo from './Logo/index'
 
+function Home() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const name = ['i', 'b', 'a', 'm', 'l', 'a', 'k']
+  const job = ["w",'e','b', ' ','d','e','v','e','l','o','p','e','r', '.']
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+    <div className='container home-page'>
+        <div className="text-zone">
+            <h1>
+              <span className={letterClass}>H</span> 
+              <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="Developer" />
+            <AnimatedLetters letterClass={letterClass} letters={name} index={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} letters={job} index={22} />
+            </h1>
+            <h2>Frontend Developer / JavaScript Expert</h2>
+            <Link to='/contact' className='flat-button'>Contact Me</Link>
+        </div>
+        <Logo />
+    </div>
+    <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
