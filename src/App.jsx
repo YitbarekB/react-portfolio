08865e23747d8c63f329b28404ee81acac94c5ed
Yitbarek Birhanie <47268052+YitbarekB@users.noreply.github.com>
2022-04-19 14:15:33 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import './App.scss'
+import About from './pages/About'
 
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
